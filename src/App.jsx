7cc1107b2d1316fe38838935790db515aaf1f566
Kartik Bhadane <kartikbhadane28@gmail.com>
2025-09-01 16:51:43 +0530

    Header added
    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import ProductPage from './Pages/Product/productpage';
-
+import Header from './Components/Header/Header.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <div className="App">
-      <ProductPage />
+    <div>
+      <Header />
     </div>
-      
-    </>
   )
 }
 
