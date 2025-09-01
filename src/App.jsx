d45685314d11754c35b5f5af5450feb47ed1b0d3
Kartik Bhadane <kartikbhadane28@gmail.com>
2025-09-01 18:59:43 +0530

    header addeed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './Components/Header/Header.jsx'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Footer from "./Components/Footer/Footer";
 
+function App() {
   return (
     <div>
       <Header />
+      <div style={{ height: "80vh", background: "linear-gradient(#8e2de2, #c94b4b)" }}>
+        {/* Demo content */}
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
